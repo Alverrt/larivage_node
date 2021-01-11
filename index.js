f2e7b1b885ac00bebcf16cf24017b060797e9637
Alverrt <batuhannbirkan@gmail.com>
2021-01-12 02:51:18 +0300

    add navbar and login page

--- index.js
+++ index.js
@@ -2,8 +2,18 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.static('public'))
+app.set('view engine', 'ejs')
+app.set('views', process.cwd() + '/views')
+app.use(express.urlencoded({ extended: false }))
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render('login/login.ejs')
+})
+
+// giris yabmadiysa giremez kontrolu yap
+app.get('/rolebase', (req, res) => {
+  res.render('rolebase/chooseRole.ejs')
 })
 
 app.listen(port, () => {
