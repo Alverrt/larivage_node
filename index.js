762897821bf58d8cb0191915c15a20e3924323d0
Alverrt <batuhannbirkan@gmail.com>
2021-01-28 16:26:04 +0300

    cookie-session add

--- index.js
+++ index.js
@@ -1,23 +1,46 @@
 const express = require('express')
+const cookieSession = require('cookie-session')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
-app.use(express.static('public'))
+app.use(express.static(__dirname + '/public', {redirect: false} ))
 app.set('view engine', 'ejs')
 app.set('views', process.cwd() + '/views')
 app.use(express.urlencoded({ extended: false }))
 
+app.use(cookieSession({
+  name: 'guid',
+  secret: 'albertino'
+}))
+
+const checkSession = (req, res, next) => {
+  if (!req.session.guid) {
+    res.redirect('/')
+  }
+  next()
+} 
+
+
 app.get('/', (req, res) => {
+  /*if (req.session.guid) {
+    res.redirect('/rolebase') //prodda aktif et
+  }*/
   res.render('login/login.ejs')
 })
 
 // giris yabmadiysa giremez kontrolu yap
-app.get('/rolebase', (req, res) => {
+app.get('/rolebase', checkSession, (req, res) => {
   res.render('rolebase/chooseRole.ejs')
 })
 
-app.get('/roleprofile', (req, res) => {
+app.get('/roleprofile', checkSession, (req, res) => {
   res.render('roleprofile/roleprofile.ejs')
+})
+
+app.post('/login', (req, res) => {
+  req.session.guid = req.body.guid
+  res.redirect('/rolebase')
 })
 
 app.listen(port, () => {
